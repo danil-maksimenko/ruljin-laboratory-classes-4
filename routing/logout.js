06902a56be3b1d23ec623a9c2b007ffc36bf2e1f
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-04-15 17:20:40 +0200

    test

--- routing/logout.js
+++ routing/logout.js
@@ -1,16 +1,11 @@
 const express = require("express");
-
-const { LOGOUT_LINKS } = require("../constants/navigation");
-
 const router = express.Router();
+const {
+  getLogoutView,
+  killApplication,
+} = require("../controllers/logoutController");
 
-router.get("/", (_request, response) => {
-  response.render("logout.ejs", {
-    headTitle: "Shop - Logout",
-    path: "/logout",
-    activeLinkPath: "/logout",
-    menuLinks: LOGOUT_LINKS,
-  });
-});
+router.get("/", getLogoutView);
+router.post("/kill", killApplication);
 
 module.exports = router;
